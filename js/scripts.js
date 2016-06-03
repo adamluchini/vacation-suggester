c3a2a22de0fca05b3ca190c64ba75cf5b60593a8
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-03 11:31:22 -0700

    trims query. removes excess commands

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,31 @@ $(document).ready(function() {
   var drinks = $("select#drinks").val();
   var passport = $("select#passport").val();
 
-  if (distance === "far" && setting === "desolate" && company === ("romantic" || "friends") && drinks === ("booze" || "tea") && passport === "pp") {
+  if (distance === "far" && setting === "desolate" && passport === "pp" ) {
   $("#foam-lake").show();
   }
 
-  else if (distance === "far" && setting === "urban" && company === ("romantic" || "friends" ) && drinks === ("booze" || "tea") && passport === "no-pp") {
-  $("#chicago").show();
+  else if (distance === "close" && setting === "desolate" && drinks === "tea") {
+  $("#mt-hood").show();
   }
 
-  else if (distance === "close" && setting === "desolate" && company === ("romantic" || "friends") && drinks === ("booze" || "tea") && passport === ("pp" || "no-pp")) {
-  $("#bend").show();
+  else if (distance === "far" && setting === "urban" && company === "friends" && drinks === "booze") {
+  $("#reno").show();
   }
 
-  else if (distance === "far" && setting === ("urban" || "desolate") && company === ("romantic" || "friends") && drinks === ("booze" || "tea") && passport === "pp") {
+  else if (distance === "far" && setting === "urban" && passport === "pp") {
   $("#brit-columbia").show();
   }
 
-  else if (distance === "close" && setting === ("urban" || "desolate") && company === ("romantic" || "friends") && drinks === ("booze" || "tea") && passport === ("pp" || "no-pp")) {
+  else if (distance === "close" && setting === "desolate") {
+  $("#bend").show();
+  }
+
+  else if (distance === "far" && setting === "urban") {
+  $("#chicago").show();
+  }
+
+  else if (distance === "close") {
   $("#portland").show();
   }
 

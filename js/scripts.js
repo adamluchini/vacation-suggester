aa67ef313c8ba8aec6446213f02b2f7a8a274952
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-03 15:32:52 -0700

    hide functions to cities not being displayed

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,72 @@ $(document).ready(function() {
     if (distance === "far" && setting === "desolate" && passport === "pp" ) {
     $("#foam-lake").show();
     $("#mt-hood").hide();
+    $("#portland").hide();
+    $("#reno").hide();
+    $("#bend").hide();
+    $("#chicago").hide();
+    $("#brit-columbia").hide();
+
     }
 
     else if (distance === "close" && setting === "desolate" && drinks === "tea") {
     $("#mt-hood").show();
+    $("#foam-lake").hide();
+    $("#portland").hide();
+    $("#reno").hide();
+    $("#bend").hide();
+    $("#chicago").hide();
+    $("#brit-columbia").hide();
     }
 
     else if (distance === "far" && setting === "urban" && company === "friends" && drinks === "booze") {
     $("#reno").show();
+    $("#foam-lake").hide();
+    $("#portland").hide();
+    $("#mt-hood").hide();
+    $("#bend").hide();
+    $("#chicago").hide();
+    $("#brit-columbia").hide()
     }
 
     else if (distance === "far" && setting === "urban" && passport === "pp") {
     $("#brit-columbia").show();
+    $("#foam-lake").hide();
+    $("#portland").hide();
+    $("#mt-hood").hide();
+    $("#bend").hide();
+    $("#chicago").hide();
+    $("#reno").hide()
     }
 
     else if (distance === "close" && setting === "desolate") {
     $("#bend").show();
+    $("#foam-lake").hide();
+    $("#portland").hide();
+    $("#mt-hood").hide();
+    $("#brit-columbia").hide();
+    $("#chicago").hide();
+    $("#reno").hide()
     }
 
     else if (distance === "far" && setting === "urban") {
     $("#chicago").show();
+    $("#foam-lake").hide();
+    $("#portland").hide();
+    $("#mt-hood").hide();
+    $("#brit-columbia").hide();
+    $("#bend").hide();
+    $("#reno").hide()
     }
 
     else if (distance === "close") {
     $("#portland").show();
+    $("#foam-lake").hide();
+    $("#chicago").hide();
+    $("#mt-hood").hide();
+    $("#brit-columbia").hide();
+    $("#bend").hide();
+    $("#reno").hide()
     }
 
     else {

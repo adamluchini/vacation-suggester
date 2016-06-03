8a63a6f22095cd15478bdc483acc0353a5e828d7
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-03 15:17:46 -0700

    foam lake hids mt hood

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,40 @@ $(document).ready(function() {
   var drinks = $("select#drinks").val();
   var passport = $("select#passport").val();
 
-  if (distance === "far" && setting === "desolate" && passport === "pp" ) {
-  $("#foam-lake").show();
-  }
+    if (distance === "far" && setting === "desolate" && passport === "pp" ) {
+    $("#foam-lake").show();
+    $("#mt-hood").hide();
+    }
 
-  else if (distance === "close" && setting === "desolate" && drinks === "tea") {
-  $("#mt-hood").show();
-  }
+    else if (distance === "close" && setting === "desolate" && drinks === "tea") {
+    $("#mt-hood").show();
+    }
 
-  else if (distance === "far" && setting === "urban" && company === "friends" && drinks === "booze") {
-  $("#reno").show();
-  }
+    else if (distance === "far" && setting === "urban" && company === "friends" && drinks === "booze") {
+    $("#reno").show();
+    }
 
-  else if (distance === "far" && setting === "urban" && passport === "pp") {
-  $("#brit-columbia").show();
-  }
+    else if (distance === "far" && setting === "urban" && passport === "pp") {
+    $("#brit-columbia").show();
+    }
 
-  else if (distance === "close" && setting === "desolate") {
-  $("#bend").show();
-  }
+    else if (distance === "close" && setting === "desolate") {
+    $("#bend").show();
+    }
 
-  else if (distance === "far" && setting === "urban") {
-  $("#chicago").show();
-  }
+    else if (distance === "far" && setting === "urban") {
+    $("#chicago").show();
+    }
 
-  else if (distance === "close") {
-  $("#portland").show();
-  }
+    else if (distance === "close") {
+    $("#portland").show();
+    }
 
-  else {
-    alert ("I need some more information");
-  }
+    else {
+      alert ("I need some more information");
+    }
+
+
   event.preventDefault();
  });
 });

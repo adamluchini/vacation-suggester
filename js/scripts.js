68369d9a1c9e806b048364d0d5713c6821275e77
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-03 09:44:04 -0700

    all 5 questions, correct string for foam lake

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
   var distance = $("select#distance").val();
-  var culture = $("select#culture").val();
+  var setting = $("select#setting").val();
+  var company = $("select#company").val();
+  var drinks = $("select#drinks").val();
+  var passport = $("select#passport").val();
 
-  if(distance==="close" && culture==="urban" || "desolate") {
+  if (distance === "far" && setting === "desolate" && company === "romantic" || "friends" && drinks === "booze" || "tea" && passport === "pp"){
+  $("#foam-lake").show();
+  }
+
+  else if (distance === "close" && setting === "desolate") {
+  $("#bend").show();
+  }
+
+  else if (distance === "close" && setting === "urban") {
   $("#portland").show();
- }
-
+  }
 
   event.preventDefault();
  });
